7a15570aaaba52e0a46ebbfef86bf83fe0b60283
ele1000 <ham_lover@126.com>
2017-06-20 22:46:42 +0000

    apply realtime patch

--- include/linux/radix-tree.h
+++ include/linux/radix-tree.h
@@ -230,7 +230,10 @@ unsigned long radix_tree_next_hole(struct radix_tree_root *root,
 				unsigned long index, unsigned long max_scan);
 unsigned long radix_tree_prev_hole(struct radix_tree_root *root,
 				unsigned long index, unsigned long max_scan);
+
 int radix_tree_preload(gfp_t gfp_mask);
+void radix_tree_preload_end(void);
+
 void radix_tree_init(void);
 void *radix_tree_tag_set(struct radix_tree_root *root,
 			unsigned long index, unsigned int tag);
@@ -252,11 +255,6 @@ unsigned long radix_tree_range_tag_if_tagged(struct radix_tree_root *root,
 		unsigned int fromtag, unsigned int totag);
 int radix_tree_tagged(struct radix_tree_root *root, unsigned int tag);
 unsigned long radix_tree_locate_item(struct radix_tree_root *root, void *item);
-
-static inline void radix_tree_preload_end(void)
-{
-	preempt_enable();
-}
 
 /**
  * struct radix_tree_iter - radix tree iterator state
